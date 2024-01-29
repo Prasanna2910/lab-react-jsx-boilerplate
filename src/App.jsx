60717aab27fb440d04bd3b0ea6ac0256fce43d87
Prasanna V <you@example.com>
2024-01-29 16:39:59 +0530

    coded

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,21 @@ const imageData = ()=>{
 
 function App() {
   // code here
+  let Data = imageData()
+  return (
+    <div>
+      <div>
+        <h1 className='heading' >Kalvium Gallery-1</h1>
+      </div>
+      <div className='img-container'>
+        {Data.map((element) => {
+          return <img src={element.img} />
+        })
+
+        }
+      </div>
+    </div>
+  )
 }
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -1,30 +1,40 @@
-import { Component } from "react";
-import "./App.css"
-import elephant from "./images/elephant.jpeg";
+import { Component } from 'react';
+import './App.css';
+import elephant from './images/elephant.jpeg';
 
-export default class AppClass extends Component{
-  
-  imageData = ()=>{
-    let data = [
-      {
-        id:1,
-        img:elephant
-      },
-      {
-        id:2,
-        img:elephant
-      },
-      {
-        id:3,
-        img:elephant
-      },
-      {
-        id:4,
-        img:elephant
-      }
-    ]
-    return data;
-  }
-
-  // code here
+const imgData = () => {
+  let data = [
+    {
+      id: 1,
+      img: elephant,
+    },
+    {
+      id: 2,
+      img: elephant,
+    },
+    {
+      id: 3,
+      img: elephant,
+    },
+    {
+      id: 4,
+      img: elephant,
+    },
+  ];
+  return data;
+};
+// code here
+function App() {
+  let Data = imgData();
+  return (
+    <div>
+      <h1 className="heading">Kalvium Gallery</h1>
+      <div className="image-container">
+        {Data.map((element) => {
+          return <img src="{element.img" />;
+        })}
+      </div>
+    </div>
+  );
 }
+export default App;
